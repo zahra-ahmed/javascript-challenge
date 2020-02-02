1bc82d83eb429e9a5f83b0e81915a6567ee9e085
Syeda Zahra Ahmed <Zahra@Syedas-MacBook-Air.local>
2020-02-01 22:05:29 -0500

    complete level 1

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+// d3 to select body
+var tbody = d3.select("tbody")
+
+tableData.forEach(function(dataLoop){
+    // row
+    var row = tbody.append("tr");
+    // data in row
+    Object.entries(dataLoop).forEach(function([key,value]){
+        console.log(key,value);
+        var cell = row.append("td").text(value);
+    });
+});
+
+
+//d3 to select button
+var btn = d3.select("#filter-btn")
+
+btn.on("click", function(){
+    // datetime variables
+    var dt = d3.select('#datetime');
+    var sel_dt = dt.property("value");
+
+    // clear table
+    tbody.html("")
+
+    // variable to show filtered data only
+    var filteredData = tableData.filter(dataLoop => dataLoop.datetime === sel_dt);
+
+    filteredData.forEach(function(dataLoop){
+        // row
+        var row = tbody.append("tr");
+        // data in row
+        Object.entries(dataLoop).forEach(function([key,value]){
+            console.log(key,value);
+            var cell = row.append("td").text(value);
+        });
+    });
+})
